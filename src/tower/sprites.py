63b8b33c4240b2916dea4f0fd80a35f9cbe4b900
stickie <usethecorrectformat@gmail.com>
2023-03-17 00:02:36 +1100

    Sprite stuff - Added AttackSpec class for attacks - Created Tower and Enemy sprite classes

---
+++ src/tower/sprites.py
@@ -0,0 +1,68 @@
+"""
+Sprites
+"""
+
+
+from pathlib import Path
+
+import arcade
+from attrs import define, field
+
+
+@define(slots=True, frozen=True)
+class AttackSpec:
+    """Class for defining an attack"""
+
+    name: str
+    desc: str
+    base_atk_damage: float = field(converter=float)
+    base_atk_speed: float = field(converter=float)
+    base_proj_speed: float = field(converter=float)
+
+    def get_sprite_path(self) -> Path:
+        return Path(__file__).parent.parent.parent / "assets" / "sprites" / "projectiles" / f"{self.name.lower()}.png"
+
+    # These three should scale off of level
+    # Proper scaling can come later after we actually implement gameplay
+    def atk_damage(self, level: int) -> float:
+        return self.base_atk_damage * level
+
+    def atk_speed(self, level: int) -> float:
+        return self.base_atk_speed * level
+
+    def proj_speed(self, level: int) -> float:
+        return self.base_proj_speed * level
+
+
+class Tower(arcade.Sprite):
+    """Tower sprite"""
+
+    def __init__(
+        self, name: str, desc: str, level: int, attacks: list[AttackSpec], scale: int = 1
+    ):  # IDK if scale is important but it's in the docs
+        """Tower constructor"""
+
+        self.name = name
+        self.desc = desc
+        self.level = level
+
+        self.sprite_path = Path(__file__).parent.parent.parent / "assets" / "sprites" / "towers" / f"{name}.png"
+
+        self.attacks = attacks
+
+        super().__init__(self.sprite_path, scale, hit_box_algorithm=None)
+
+
+class Enemy(arcade.Sprite):
+    """Enemy sprite"""
+
+    def __init__(self, name: str, desc: str, speed: float, scale: int = 1):
+        """Enemy constructor"""
+
+        self.name = name
+        self.desc = desc
+        self.speed = speed
+
+        self.sprite_path = Path(__file__).parent.parent.parent / "assets" / "sprites" / "enemies" / f"{name}.png"
+
+        super().__init__(self.sprite_path, scale, hit_box_algorithm=None)
