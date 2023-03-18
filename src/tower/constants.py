2de514b6e74f16c2c09f488853a35e0e7c328c45
Henri <96082972+henri-climber@users.noreply.github.com>
2023-03-18 10:01:08 +0100

    Merge "main" into enemyMovement

--- src/tower/constants.py
+++ src/tower/constants.py
@@ -5,11 +5,12 @@ SCREEN_WIDTH = 24 * 64
 SCREEN_HEIGHT = 15 * 64
 SCREEN_TITLE = "Platformer"
 
-# ID's of our path start sprites
-START_SPRITE_IDS = {5}
+# ID's of our path start sprites in the data csv
+START_SPRITE_IDS = {3}
 
-# ID's of our path end sprites
-END_SPRITE_IDS = {5}
+# ID's of our path end sprites of sprite.propertie[tile_id]
+# for some reason the tile_id is one lower than its representation in the data csv
+END_SPRITE_IDS = {3}
 
-# ID's of all path blocks
-PATH_SPRITE_IDS = {3, 6, 5}
+# ID's of all path sprites in the data csv
+PATH_SPRITE_IDS = {2, 4, 3}
