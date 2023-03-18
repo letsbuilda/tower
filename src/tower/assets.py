b2dbe86293ca73cffb782172088a1c13662b2491
stickie <usethecorrectformat@gmail.com>
2023-03-19 01:34:08 +1100

    Added ability for towers to shoot - Added fireball.png - Made a coordinate function in assets - Added tile dimension constants - Added enemy health - Added projectile sprite class - Added tower shooting

--- src/tower/assets.py
+++ src/tower/assets.py
@@ -3,6 +3,8 @@
 from importlib.abc import Traversable
 from importlib.resources import files
 
+from .constants import MAP_WIDTH
+
 
 def get_asset_path(*paths: str) -> Traversable:
     """Gets the path for an asset"""
@@ -20,3 +22,8 @@ def get_sprite_path(parent: str, name: str) -> Traversable:
 def get_tile_map_path(name: str) -> Traversable:
     """Gets the path for a tile map"""
     return get_asset_path("levels", name)
+
+
+def coord_to_index(x_coord: int, y_coord: int) -> int:
+    """Turns an (x, y) coordinate into an index for the tile map"""
+    return MAP_WIDTH * x_coord + y_coord
--- src/tower/sprites.py
+++ src/tower/sprites.py
@@ -25,9 +25,9 @@ class AttackSpec:
         """Calculates the damage of the attack"""
         return self.base_atk_damage * level
 
-    def atk_speed(self, level: int) -> float:
+    def atk_cooldown(self, level: int) -> float:
         """Calculates the cooldown of the attack"""
-        return self.base_atk_cooldown * level
+        return self.base_atk_cooldown / level
 
     def proj_speed(self, level: int) -> float:
         """Calculates the speed of the projectile"""
@@ -50,21 +50,35 @@ class Enemy(arcade.Sprite):
     """Enemy sprite"""
 
     # pylint: disable=too-many-arguments
-    def __init__(self, name: str, desc: str, speed: float, position_list: list, scale: int = 1):
+    def __init__(self, name: str, desc: str, speed: float, health: int, position_list: list, scale: int = 1):
         """Enemy constructor"""
         self.name = name
         self.desc = desc
         self.speed = speed
+        self._health = health
         self.position_list = position_list
         self.current_position = 0
         self.is_alive = True
 
         with as_file(get_sprite_path("enemies", f"{name}.png")) as file_path:
-            super().__init__(file_path, scale, hit_box_algorithm=None)
+            super().__init__(file_path, scale)
+
+    @property
+    def health(self):
+        """Health getter"""
+        return self._health
+
+    @health.setter
+    def health(self, value: int):
+        """Health setter"""
+        if value <= 0:
+            self.is_alive = False
+        self._health = value
 
     def update(self):
         """calculates new position of enemy but only when it is alive"""
         if not self.is_alive:
+            self.remove_from_sprite_lists()
             return
         # current position
         start_x = self.center_x
@@ -105,13 +119,17 @@ class Tower(arcade.Sprite):
 
     # pylint: disable-next=too-many-arguments
     def __init__(
-        self, name: str, desc: str, level: int, attacks: list[AttackSpec], scale: int = 1, radius: int = 100
+        self, name: str, desc: str, level: int, attacks: list[AttackSpec], radius: int = 100, scale: int = 1
     ):  # IDK if scale is important but it's in the docs
         """Tower constructor"""
         self.level = level
         self.radius = radius
         self.desc = desc
-        self.attacks = attacks
+        self.attack_cooldowns = {attack: 0 for attack in attacks}
+        self.current_target = None
+
+        # Add attacks here to be processed later in GameView
+        self.pending_attacks = []
 
         with as_file(get_sprite_path("towers", f"{name}.png")) as file_path:
             super().__init__(file_path, scale, hit_box_algorithm=None)
@@ -120,9 +138,61 @@ class Tower(arcade.Sprite):
         """Shows the range of the tower"""
         arcade.draw_circle_outline(self.center_x, self.center_y, self.radius, arcade.color.BLACK, 2)
 
-    def attack(self, enemy: Enemy):
+    def attack(self, attack: AttackSpec):
         """
-        Initiates an attack on the enemy when in circle range
-        To do this all we need to do is check if the distance
-        between the enemy and the tower is less than or equal to the radius
+        Fires an attack at self.current_target
+        Assumes target is in range
         """
+        self.pending_attacks.append(Projectile((self.center_x, self.center_y), attack, self.level, self.current_target))
+
+    def get_enemy_dist(self, enemy: Enemy) -> float:
+        """Get distance from tower to enemy"""
+        return math.dist(self.position, enemy.position)
+
+    def update(self):
+        """
+        Updates attacks on target
+        Assumes target is in range
+        """
+        if not self.current_target:
+            return
+        for attack, cooldown in self.attack_cooldowns.items():
+            # Cooldown is over
+            if not cooldown:
+                self.attack(attack)
+                self.attack_cooldowns[attack] = attack.atk_cooldown(self.level)
+            # Decrement cooldown
+            else:
+                self.attack_cooldowns[attack] -= 1
+
+
+class Projectile(arcade.Sprite):
+    """Projectile sprite"""
+
+    # pylint: disable-next=too-many-arguments
+    def __init__(self, position: tuple[float, float], attack: AttackSpec, level: int, target: Enemy, scale: int = 0.2):
+        """Projectile constructor"""
+        self.attack = attack
+        self.target = target
+        self.speed = attack.proj_speed(level)
+        self.level = level
+
+        with as_file(get_sprite_path("projectiles", f"{self.attack.name}.png")) as file_path:
+            super().__init__(file_path, scale)
+
+        self.center_x, self.center_y = position
+
+    def get_enemy_dist(self, enemy: Enemy) -> float:
+        """Gets distance from projectile to enemy"""
+        return math.dist(self.position, enemy.position)
+
+    def update(self):
+        """Update function"""
+        # If it has hit the target
+        if self.collides_with_point(self.target.position):
+            self.target.health -= self.attack.atk_damage(self.level)
+            self.remove_from_sprite_lists()
+
+        angle = math.atan2(self.target.center_y - self.center_y, self.target.center_x - self.center_x)
+        self.velocity = self.speed * math.cos(angle), self.speed * math.sin(angle)
+        super().update()
